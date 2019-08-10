cacfc54559ab0346e379afb118ef52a7bef7f0e5
ganeshblaze <argentinamark@gmail.com>
2019-08-10 13:14:32 +0530

    Updates

---
+++ precache-manifest.f8854294574212fc409df27993159efc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3095a4b88e1d7bfc0e4150223f84ad69",
+    "url": "/audioplayer/index.html"
+  },
+  {
+    "revision": "3fa36190589031dc797a",
+    "url": "/audioplayer/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/audioplayer/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "3fa36190589031dc797a",
+    "url": "/audioplayer/static/js/main.d13fff5e.chunk.js"
+  },
+  {
+    "revision": "1b01da2685130aa0cd94",
+    "url": "/audioplayer/static/js/runtime~main.2f0c08f4.js"
+  }
+]);
